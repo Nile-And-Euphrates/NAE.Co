69a292fca028576d7a422f0d511093dadba9abf0
muhammed maher <muhammedmaher@gmail.com>
2024-08-30 08:03:20 +0300

    structure files has updated

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -15,11 +15,11 @@ export default function Works(){
 
     const [data,setData]=useState([])
     useEffect(()=>{
-        const token = '0620d589690987cf46c668b6b66f3eb3b90bd3c3';
+        const token = 'a7855b87ede8e3c34112928a1384d2e1ce312806';
         fetch('http://127.0.0.1:8000/restframework/generics/', {
             method: 'GET',
             headers: {
-            'Authorization': 'Bearer ' + token,
+            'Authorization':`Token ${token}`,
             'Content-Type': 'application/json'
             },
         })
